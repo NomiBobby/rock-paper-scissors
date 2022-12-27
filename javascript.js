3915ae2b1d54ec09b5f5f1d365c9416b0fe90778
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2022-12-26 22:19:21 -0500

    add restart game button

--- javascript.js
+++ javascript.js
@@ -16,47 +16,94 @@ function playRound(playerSelection, computerSelection) {
 
     if(playerSelection === "rock"){
         if(computerSelection ==="rock"){
-            return `Another round! ${playerSelection} ties with ${computerSelection}`
+            return 0;
         }
         if(computerSelection ==="paper"){
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
+            return 1;
         }
         if(computerSelection ==="scissors"){
-            return `You Win! ${playerSelection} beats ${computerSelection}`
+            return 2;
         }
 
     else if(playerSelection === "paper"){
         if(computerSelection ==="paper"){
-            return `Another round! ${playerSelection} ties with ${computerSelection}`
+            return 0;
         }
         if(computerSelection ==="scissors"){
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
+            return 1;
         }
         if(computerSelection ==="rock"){
-            return `You Win! ${playerSelection} beats ${computerSelection}`
+            return 2;
         }
     }
 
     else if(playerSelection === "scissors"){
         if(computerSelection ==="scissors"){
-            return `Another round! ${playerSelection} ties with ${computerSelection}`
+            return 0;
         }
         if(computerSelection ==="rock"){
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
+            return 1;
         }
         if(computerSelection ==="paper"){
-            return `You Win! ${playerSelection} beats ${computerSelection}`
+            return 2;
         }
     }
     }
 }
 
-function game(){
-    const playerSelection = "rock";
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        console.log(`Round ${i}, ${playRound(playerSelection, computerSelection)}`);
+function restartGame(e){
+    counterHuman = 0;
+    counterComputer = 0;
+    counter = 0;
+    document.getElementById("computerScore").innerHTML = counterComputer;
+    document.getElementById("humanScore").innerHTML = counterHuman;
+    console.log(e);
+    let btnParent = e.target.parentNode;
+    while (btnParent.firstChild){
+        btnParent.removeChild(btnParent.lastChild);
     }
 }
 
-game();
+function game(e){
+    if(counter == 4) {
+        if(counterHuman > counterComputer) {
+            document.getElementById("result").innerHTML = "Human wins!";
+        }
+        else{
+            document.getElementById("result").innerHTML = "Computer wins!";
+        }
+        let btn = document.createElement("button");
+        btn.innerHTML = "Restart Game";
+        btn.addEventListener("click", restartGame);
+        document.getElementById("result").appendChild(btn);
+        counter++;
+        return;
+    }
+    else if(counter >4){
+        return;
+    };
+    const playerSelection = "rock";
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    if (result === 0){
+        return;
+    }
+    else if(result === 1){
+        counterComputer++;
+        // console.log(`counterComputer: ${counterComputer}`)
+    }
+    else if (result === 2){
+        counterHuman++;
+        // console.log(`counterHuman: ${counterHuman}`)
+    }
+    document.getElementById("computerScore").innerHTML = counterComputer;
+    document.getElementById("humanScore").innerHTML = counterHuman;
+    counter++;
+}
+
+// Main Code
+let counterHuman = 0;
+let counterComputer = 0;
+let counter = 0;
+let buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach(button => button.addEventListener("click", ()=>game()));
